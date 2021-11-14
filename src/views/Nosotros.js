08a0a58ea1a0b50d75d0f705f47bff573dc4c57d
ManuDavril31 <manumar451@gmail.com>
2021-11-14 02:03:30 -0500

    vista nosotros

--- src/views/Nosotros.js
+++ src/views/Nosotros.js
@@ -1,3 +1,24 @@
+import "../views/nosotros.css";
+
 export default function Nosotros() {
-  return <p>hola</p>;
+  return (
+    <div className="row justify-content-center align-items-center p-5 fondo3 text-white">
+      <div className="col-12">
+        <h1>NOSOTROS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A vitae
+          minima, sint eaque quam accusantium cupiditate quaerat aspernatur,
+          modi corrupti natus, voluptate numquam. Autem reiciendis voluptatum
+          deserunt veniam dolores maxime? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. In minima saepe quasi illo, ipsam labore ad esse
+          earum neque, hic, provident reprehenderit! Praesentium laboriosam
+          voluptates iure corporis repudiandae, nostrum suscipit! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Est minima modi esse
+          quidem accusamus molestiae quod, repellendus cum voluptas voluptatem
+          debitis aliquam deserunt ut ipsam perspiciatis corrupti, omnis
+          aspernatur ipsa.
+        </p>
+      </div>
+    </div>
+  );
 }
